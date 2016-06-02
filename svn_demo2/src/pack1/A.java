a126da0ca192af0e1f84b49d69c62c561454b5de
shivangi.kanodia <shivangi.kanodia@BDC4A-D-74MSX52.dir.svc.accenture.com>
2016-06-02 12:25:10 +0530

    message1-added

---
+++ svn_demo2/src/pack1/A.java
@@ -0,0 +1,11 @@
+package pack1;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           System.out.println("message-1");
+           System.out.println("message-2");
+	}
+
+}
